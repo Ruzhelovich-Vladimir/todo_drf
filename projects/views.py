322f1b9763b1e3c1c22a3a1827984b903ab68be6
Vladimir Ruzhelovich <Ruzhelovich.Vladimir@gmail.com>
2022-03-27 22:45:26 +0600

    lesson-4

--- projects/views.py
+++ projects/views.py
@@ -13,6 +13,7 @@ class ProjectModelViewSet(ModelViewSet):
 
     queryset = Project.objects.all() #.filter(is_activate=True)
     serializer_class = ProjectModelSerializer
+    filterset_fields = ['name']
     pagination_class = ProjectLimitOffsetPagination
 
     def destroy(self, request, *args, **kwargs):
@@ -24,21 +25,21 @@ class ProjectModelViewSet(ModelViewSet):
             pass
         return Response(status=status.HTTP_200_OK)
 
-    def get_queryset(self):
-        queryset = Project.objects.all() #.filter(is_activate=True)
-        pagination_class = ProjectLimitOffsetPagination
-
-        # Фильтр в запросе
-        if 'name' in self.request.query_params:
-            # В параметрах может быть только несколько значений
-            for search_elem in self.request.query_params['name']:
-                queryset = queryset.filter(name__contains=search_elem)
-        # Фильтр в заголовке запроса
-        if 'name' in self.request.headers:
-            # В заголовке может быть только одно значение
-            queryset = queryset.filter(
-                name__contains=self.request.headers['name'])
-        return queryset
+    # def get_queryset(self):
+    #     queryset = Project.objects.all() #.filter(is_activate=True)
+    #     pagination_class = ProjectLimitOffsetPagination
+    #
+    #     # Фильтр в запросе
+    #     if 'name' in self.request.query_params:
+    #         # В параметрах может быть только несколько значений
+    #         for search_elem in self.request.query_params['name']:
+    #             queryset = queryset.filter(name__contains=search_elem)
+    #     # Фильтр в заголовке запроса
+    #     if 'name' in self.request.headers:
+    #         # В заголовке может быть только одно значение
+    #         queryset = queryset.filter(
+    #             name__contains=self.request.headers['name'])
+    #     return queryset
 
 
 class DevelopmentModelViewSet(ModelViewSet):
